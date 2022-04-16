582950d7d502448a5c6e61bb2ce81c0d42eaeec5
yaclaudes1 <yaclaudes1@hotmail.com>
2022-04-16 17:01:53 -0400

    Correctness on timecheck validated.

--- commands/timecheck.js
+++ commands/timecheck.js
@@ -40,7 +40,7 @@ module.exports = {
         const previousNengBlockHash = 'http://nengexplorer.mooo.com:3001/api/getblock?hash=' + blockHashArray[2];
         const previousChtaBlockHash = 'http://chtaexplorer.mooo.com:3002/api/getblock?hash=' + blockHashArray[3];
 
-        const timeRegEx = (/"time": \d+/i);
+        const timeRegEx = (/"mediantime": \d+/i);
         const filterLetterRegEx = (/\D/g);
 
         const getCurrentNengBlockHashInfoAPI = fetch(currentNengBlockHash).then(response => response.text()).then(extractTime => extractTime.match(timeRegEx)).then(keepNumbers => keepNumbers.toString().replace(filterLetterRegEx, ''));
@@ -50,8 +50,8 @@ module.exports = {
 
         const filteredTimeArray = await Promise.all([getCurrentNengBlockHashInfoAPI, getCurrentChtaBlockHashInfoAPI, getPreviousNengBlockHashInfoAPI, getPreviousChtaBlockHashInfoAPI]);
         //in days
-        const nengTimeTwentyLastTwentyBlocks = ((filteredTimeArray[0] - filteredTimeArray[2]) / (1000));
-        const chtaTimeTwentyLastTwentyBlocks = ((filteredTimeArray[1] - filteredTimeArray[3]) / (1000));
+        const nengTimeTwentyLastTwentyBlocks = Math.floor(((filteredTimeArray[0] - filteredTimeArray[2]) / 60));
+        const chtaTimeTwentyLastTwentyBlocks = Math.floor(((filteredTimeArray[1] - filteredTimeArray[3]) / 60));
 
         // ***DONE***S1 fetch Block height and calculate prior 20.
         // ***DONE***S2 ReturnblockHash
@@ -59,13 +59,12 @@ module.exports = {
         // ***DONE***+***Temp Workaround used regexp to process response.text() + splice string method ***S4 parse JSON and keep time 
         // ***DONE***S5 Get time of currentBlockTime - time of currentBlockTimeMinusTwenty
         // ***DONE***Echo info from S5 as reply in human readable language
-        interaction.editReply('Neng solve time for the previous 20 blocks is: ' + nengTimeTwentyLastTwentyBlocks + ' seconds' + '\n\nChta solve time for the previous 20 blocks is: ' + chtaTimeTwentyLastTwentyBlocks) + ' seconds';
+        interaction.editReply('Neng solve time for the previous 20 blocks is: ' + nengTimeTwentyLastTwentyBlocks + ' minutes' + '\n\nChta solve time for the previous 20 blocks is: ' + chtaTimeTwentyLastTwentyBlocks + ' minutes');
         // NOTE: multiple edit replies will overwrite previous. a single reply after doesn't work in a defer reply chain.
-        // TODO: Check for correctness of time. Then write a global time function that produces readable format.
         // interaction.followUp('\n' + previousNengBlockHeightAtTwenty + '\n' + previousChtaBlockHeightAtTwenty);
         // interaction.followUp('\n' + filteredTimeArray[0] + '\n' + filteredTimeArray[1] + '\n' + filteredTimeArray[2] + '\n' + filteredTimeArray[3]);
 
 
-        // Find currentBlockIndexTime - BlockIndex[current - 20] convert into minutes to show length of time to solve last 20 blocks, repeat on both chains');
+        // Find currentBlockIndexTime - BlockIndex[current - 20] convert into minutes to show length of time ]\to solve last 20 blocks, repeat on both chains');
     },
 };
