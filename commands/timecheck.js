21ccfc44add97bd35a3db55f4e319a9a148ce09b
yaclaudes1 <yaclaudes1@hotmail.com>
2022-04-16 13:52:34 -0400

    Improved readibility

--- commands/timecheck.js
+++ commands/timecheck.js
@@ -10,7 +10,7 @@ module.exports = {
 
 
         await interaction.deferReply();
-
+        // TODO: After a working prototype is made first task towards easier to read code is to make two arrays/maps one of the specific blockchain url and the other of the specific API components such as getblockcount, etc.
         // Get current block index and place in corresponding promiseArray
         const getCurrentNengBlockHeightAPI = fetch('http://nengexplorer.mooo.com:3001/api/getblockcount').then(response => response.json());
         const getCurrentChtaBlockHeightAPI = fetch('http://chtaexplorer.mooo.com:3002/api/getblockcount').then(response => response.json());
@@ -33,12 +33,20 @@ module.exports = {
         const blockHashArray = await Promise.all([getCurrentNengBlockHashAPI, getCurrentChtaBlockHashAPI, getPreviousNengBlockHashAPI, getPreviousChtaBlockHashAPI]);
         // promiseDataArray.concat(blockHashArray);
         // Getblock[hash] info -->Return json with info relevant to timestamp
-         const getCurrentNengBlockHashInfoAPI = fetch('http://nengexplorer.mooo.com:3001/api/getblock?hash=' + blockHashArray[0]).then(response => response.text()).then(extractTime => extractTime.match(/"time": \d+/i)).then(keepNumbers => keepNumbers.toString().replace(/\D/g, ''));
-         const getCurrentChtaBlockHashInfoAPI = fetch('http://chtaexplorer.mooo.com:3002/api/getblock?hash=' + blockHashArray[1]).then(response => response.text()).then(extractTime => extractTime.match(/"time": \d+/i)).then(keepNumbers => keepNumbers.toString().replace(/\D/g, ''));
-         const getPreviousNengBlockHashInfoAPI = fetch('http://nengexplorer.mooo.com:3001/api/getblock?hash=' + blockHashArray[2]).then(response => response.text()).then(extractTime => extractTime.match(/"time": \d+/i)).then(keepNumbers => keepNumbers.toString().replace(/\D/g, ''));
-         const getPreviousChtaBlockHashInfoAPI = fetch('http://chtaexplorer.mooo.com:3002/api/getblock?hash=' + blockHashArray[3]).then(response => response.text()).then(extractTime => extractTime.match(/"time": \d+/i)).then(keepNumbers => keepNumbers.toString().replace(/\D/g, ''));
 
-         const hashRelatedInfoAPI = await Promise.all([getCurrentNengBlockHashInfoAPI, getCurrentChtaBlockHashInfoAPI, getPreviousNengBlockHashInfoAPI, getPreviousChtaBlockHashInfoAPI ]);
+        const currentNengBlockHash = 'http://nengexplorer.mooo.com:3001/api/getblock?hash=' + blockHashArray[0];
+        const currentChtaBlockHash = 'http://chtaexplorer.mooo.com:3002/api/getblock?hash=' + blockHashArray[1];
+        const previousNengBlockHash = 'http://nengexplorer.mooo.com:3001/api/getblock?hash=' + blockHashArray[2];
+        const previousChtaBlockHash = 'http://chtaexplorer.mooo.com:3002/api/getblock?hash=' + blockHashArray[3];
+
+        const timeRegEx = (/"time": \d+/i);
+        const filterLetterRegEx = (/\D/g, '');
+         const getCurrentNengBlockHashInfoAPI = fetch(currentNengBlockHash).then(response => response.text()).then(extractTime => extractTime.match(timeRegEx)).then(keepNumbers => keepNumbers.toString().replace(filterLetterRegEx));
+         const getCurrentChtaBlockHashInfoAPI = fetch(currentChtaBlockHash).then(response => response.text()).then(extractTime => extractTime.match(timeRegEx)).then(keepNumbers => keepNumbers.toString().replace(filterLetterRegEx));
+         const getPreviousNengBlockHashInfoAPI = fetch(previousNengBlockHash).then(response => response.text()).then(extractTime => extractTime.match(timeRegEx)).then(keepNumbers => keepNumbers.toString().replace(filterLetterRegEx));
+         const getPreviousChtaBlockHashInfoAPI = fetch(previousChtaBlockHash).then(response => response.text()).then(extractTime => extractTime.match(timeRegEx)).then(keepNumbers => keepNumbers.toString().replace(filterLetterRegEx));
+
+         const filteredTimeArray = await Promise.all([getCurrentNengBlockHashInfoAPI, getCurrentChtaBlockHashInfoAPI, getPreviousNengBlockHashInfoAPI, getPreviousChtaBlockHashInfoAPI ]);
 
         //
         // ***DONE***S1 fetch Block height and calculate prior 20.
@@ -50,7 +58,7 @@ module.exports = {
         await interaction.editReply('NENG current block height is : ' + promiseDataArray[0] + '\n\nCheetah current block height is: ' + promiseDataArray[1] + '\n\nNENG block current hash is : ' + blockHashArray[0] + '\n\nChta block hash is : ' + blockHashArray[1] + '\n\nNENG block hash at previous 20 blocks is: ' + blockHashArray[2] + '\n\nChta block hash at previous 20 blocks is: ' + blockHashArray[3]);
         // NOTE: multiple edit replies will overwrite previous. a single reply after doesn't work in a defer reply chain.
         await interaction.followUp('\n' + previousNengBlockHeightAtTwenty + '\n' + previousChtaBlockHeightAtTwenty);
-        await interaction.followUp('\n' + hashRelatedInfoAPI[0] + '\n' + hashRelatedInfoAPI[1] + '\n' + hashRelatedInfoAPI[2] + '\n' + hashRelatedInfoAPI[3]);
+        await interaction.followUp('\n' + filteredTimeArray[0] + '\n' + filteredTimeArray[1] + '\n' + filteredTimeArray[2] + '\n' + filteredTimeArray[3]);
        //);
 
         // Find currentBlockIndexTime - BlockIndex[current - 20] convert into minutes to show length of time to solve last 20 blocks, repeat on both chains');
