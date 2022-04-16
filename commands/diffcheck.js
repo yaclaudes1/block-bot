039d896ec45017f6c4b1c4caaef8f47f850c66ef
yaclaudes1 <yaclaudes1@hotmail.com>
2022-04-16 19:06:29 -0400

    Fixed rounding issues in diffcheck command

--- commands/diffcheck.js
+++ commands/diffcheck.js
@@ -9,11 +9,11 @@ module.exports = {
     async execute(interaction) {
 
         interaction.deferReply();
-        // TODO: Fix round off issues when a reset for neng is < 0. Show the decimal spaces with conditionals
+
         const fetchedNengApi = fetch('http://nengexplorer.mooo.com:3001/api/getdifficulty').then(response => response.json());
         const fetchedChtaApi = fetch('http://chtaexplorer.mooo.com:3002/api/getdifficulty').then(response => response.json());
         const allData = await Promise.all([fetchedNengApi, fetchedChtaApi]);
-        interaction.editReply('NENG current difficulty is: ' + Number(Math.round(allData[0])).toLocaleString() + '\nCHTA current difficulty is: ' + Number(Math.round(allData[1])).toLocaleString());
+        interaction.editReply('NENG current difficulty is: ' + Number((allData[0]).toFixed(6)).toLocaleString() + '\nCHTA current difficulty is: ' + Number((allData[1]).toFixed(6)).toLocaleString());
 
     },
 };
